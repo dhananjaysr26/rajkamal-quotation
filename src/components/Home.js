2f0122c8db47e678aab525006269dd7f2a955e6d
Dhananjay Singh <dhananjaysr26@gmail.com>
2021-08-30 04:04:15 +0530

    update Quotation Editor

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,36 @@ import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import logoPic from "./../img/logo.png";
+import { useHistory } from "react-router-dom";
 function Home() {
+    let history = useHistory();
+    function editQuotationBtn() {
+        var Qid = prompt("Please enter your Quotation ID:");
+        if (Qid != null) {
+            history.push({
+                pathname: '/quotation-editor',
+                state: Qid
+            });
+        }
+    }
     return (
         <div>
             <div className="content">
                 <a href="http://www.rajkamalbarscan.com/">
-                    <img src={logoPic} />
+                    <img src={logoPic} alt="" />
                 </a>
                 <div className="title">Welcome To Rajkamal Barscan</div>
                 <p>Get Your Quotation Now!</p>
                 <div className="btn-box">
-                    <Link to="/quotation-editor"><a className="btn">Edit Quotation</a></Link>
-                    <Link to="/quotation-editor"><a className="btn">Create Quotation</a></Link>
+                    <button className="btn" type="submit" onClick={editQuotationBtn}>Edit Quotation</button>
+
+
+                    <Link to="/quotation-editor">
+                        <button className="btn" type="button">
+                            Create Quotation
+                        </button>
+                    </Link>
+
                 </div>
             </div>
         </div>
--- src/components/QuotationEditor.js
+++ src/components/QuotationEditor.js
@@ -1,13 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import "./QuotationEditor.css";
 
-function QuotationEditor() {
+function QuotationEditor(props) {
+  //getting props
+  var Qid = props.location.state;
+  if (!Qid) {
+    Qid = Math.floor(Math.random() * 9000);
+  }
+  //amount
+  let [Amt, setAmt] = useState(1000);
+  // ******************************************************************************************************
+  //Type Prize
+  let [Ltype, setLtype] = useState(0);
+  var typeDict = {
+    "0": 0,
+    "Bar": 100,
+    "Label1": 200,
+    "Label2": 300,
+    "Label3": 400
+  };
+  //onSelect Amt Change
+  function typeChange() {
+    let selectType = document.getElementById('select-type');
+
+    setAmt(Amt + typeDict[selectType.value] - Ltype);
+    setLtype(typeDict[selectType.value]);
+
+
+  }
+  // ******************************************************************************************************
+  //Size Prize
+  let [Lsize, setLsize] = useState(0);
+  var sizeDict = {
+    "0": 0,
+    "Size1": 100,
+    "Size2": 200,
+    "Size3": 300,
+    "Size4": 400
+  };
+  //onSelect Amt Change
+  function sizeChange() {
+    let selectSize = document.getElementById('select-size');
+    setAmt(Amt + sizeDict[selectSize.value] - Lsize);
+    setLsize(sizeDict[selectSize.value]);
+
+
+  }
+  // ******************************************************************************************************
+  //Paper Prize
+  let [Lpaper, setLpaper] = useState(0);
+  var paperDict = {
+    "0": 0,
+    "Normal": 100,
+    "Paper-1": 200,
+    "Paper-2": 300,
+    "Paper-3": 400
+  };
+  //onSelect Amt Change
+  function paperChange() {
+    let selectPaper = document.getElementById('select-paper');
+
+    setAmt(Amt + paperDict[selectPaper.value] - Lpaper);
+    setLpaper(paperDict[selectPaper.value]);
+
+  }
+  // ******************************************************************************************************
+  //Ink Prize
+  let [L_ink, setL_ink] = useState(0);
+  var inkDict = {
+    "0": 0,
+    "Ink-1": 500,
+    "Ink-2": 1000
+  };
+  //onSelect Amt Change
+  function inkChange() {
+    let selectInk = document.getElementById('select-ink');
+    setAmt(Amt + inkDict[selectInk.value] - L_ink);
+    setL_ink(inkDict[selectInk.value]);
+
+  }
+  // ******************************************************************************************************
+  //Customer Prize
+  let [LcustType, setLcustType] = useState(0);
+  var custDict = {
+    "Regular": 0,
+    "OneTime": 1000
+  };
+  //onSelect Amt Change
+  function custChange() {
+    let selectCust = document.querySelectorAll('input[name="cust-type"]');
+    let selectedCustType;
+    for (const SC of selectCust) {
+      if (SC.checked) {
+        selectedCustType = SC.value;
+        break;
+      }
+    }
+    setAmt(Amt + custDict[selectedCustType] - LcustType);
+    setLcustType(custDict[selectedCustType]);
+
+  }
+
   return (
     <div>
       <form>
         <h1 className="editor">Quotation Editor</h1>
         <h2 className="amount">
-          Amount:<span className="amountNumber">125 ₹</span>
+          Amount:<span className="amountNumber">{Amt} ₹</span>
         </h2>
         <div className="form-content">
           <div className="display">
@@ -15,39 +114,42 @@ function QuotationEditor() {
               Name:
               <input type="text" />
             </div>
-            <div className="flexQ">Quotation Id:6461666</div>
+            <div className="flexQ">Quotation Id:{Qid}</div>
           </div>
           <div className="selectBox">
             <label className="label" for="type">
               Type:
             </label>
-            <select name="type" id="type">
-              <option value="Bar">Volvo</option>
-              <option value="Label">Saab</option>
-              <option value="Label2">Mercedes</option>
-              <option value="Label3">Audi</option>
+            <select name="type" id="select-type" onChange={typeChange}>
+              <option value="0">-Select-</option>
+              <option value="Bar">Bar</option>
+              <option value="Label1">Label1</option>
+              <option value="Label2">Label2</option>
+              <option value="Label3">Label3</option>
             </select>
           </div>
           <div className="selectBox">
             <label className="label" for="Size">
               Size:
             </label>
-            <select name="Size" id="Size">
-              <option value="Size0">Size1</option>
-              <option value="Sizel">Size2</option>
-              <option value="Size2">Size3</option>
-              <option value="Size3">Size4</option>
+            <select name="Size" id="select-size" onChange={sizeChange}>
+              <option value="0">-Select-</option>
+              <option value="Size1">Size1</option>
+              <option value="Size2">Size2</option>
+              <option value="Size3">Size3</option>
+              <option value="Size4">Size4</option>
             </select>
           </div>
           <div className="selectBox">
             <label className="label" for="paper">
               Paper:
             </label>
-            <select name="paper" id="paper">
-              <option value="Size0">Normal</option>
-              <option value="Sizel">Paper-2</option>
-              <option value="Size2">Paper-3</option>
-              <option value="Size3">Paper-4</option>
+            <select name="paper" id="select-paper" onChange={paperChange}>
+              <option value="0">-Select-</option>
+              <option value="Normal">Normal</option>
+              <option value="Paper-1">Paper-1</option>
+              <option value="Paper-2">Paper-2</option>
+              <option value="Paper-3">Paper-3</option>
             </select>
           </div>
 
@@ -55,11 +157,10 @@ function QuotationEditor() {
             <label className="label" for="ink">
               Ink:
             </label>
-            <select name="ink" id="ink">
-              <option value="Size0">Ink1</option>
-              <option value="Sizel">Ink2</option>
-              <option value="Size2">Ink3</option>
-              <option value="Size3">Ink4</option>
+            <select name="ink" id="select-ink" onChange={inkChange}>
+              <option value="0">-Select-</option>
+              <option value="Ink-1">Ink1</option>
+              <option value="Ink-2">Ink2</option>
             </select>
           </div>
 
@@ -67,13 +168,11 @@ function QuotationEditor() {
             <label className="label" for="Cunstomer">
               Cunstomer Type:
             </label>
-            <select name="Cunstomer" id="Cunstomer">
-              <option value="Size0">Regular</option>
-              <option value="Sizel">One Time</option>
-            </select>
+            <input type="radio" name="cust-type" value="Regular" onChange={custChange} />Regular
+            <input type="radio" name="cust-type" value="OneTime" onChange={custChange} />One Time
           </div>
           <div className="button">
-            <a href="/" className="btn">
+            <a href="#" className="btn" onClick={() => { alert("Quotation Pdf will Download!") }} disabled>
               Generate Quotation
             </a>
           </div>
