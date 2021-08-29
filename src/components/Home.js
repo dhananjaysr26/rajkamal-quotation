e206016a89d879ed0fee9778fa221bf20a2c35e3
Dhananjay Singh <dhananjaysr26@gmail.com>
2021-08-29 17:08:47 +0530

    Created img folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import logoPic from "./../img/logo.png";
 function Home() {
     return (
         <div>
             <div className="content">
                 <a href="http://www.rajkamalbarscan.com/">
-                    <img src="http://www.rajkamalbarscan.com/assets/images/logo.png" />
+                    <img src={logoPic} />
                 </a>
                 <div className="title">Welcome To Rajkamal Barscan</div>
                 <p>Get Your Quotation Now!</p>
-                <div>
-                    <Link to="/quotation-editor"><a className="btn">New Quotation</a></Link>
-                    <Link to="/quotation-editor"><a className="btn">New Quotation</a></Link>
+                <div className="btn-box">
+                    <Link to="/quotation-editor"><a className="btn">Edit Quotation</a></Link>
+                    <Link to="/quotation-editor"><a className="btn">Create Quotation</a></Link>
                 </div>
             </div>
         </div>
